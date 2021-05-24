004a7c5925807edda3f89397544439dd0be38236
Samer Araabi <samer@Samers-Mac-mini.local>
2021-05-24 12:32:23 -0700

    add production settings

---
+++ django_zimtracker/settings/production.py
@@ -0,0 +1,12 @@
+from .base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['young-anchorage-71833.herokuapp.com',]
+
+# STATIC_URL = 'https://{0}/{1}/'.format(
+#     AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# SECURE_SSL_REDIRECT = True
