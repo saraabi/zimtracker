9e931ce2c9befdd37b60e14b5118077ffeb06c96
Samer Araabi <samer@Samers-Mac-mini.local>
2021-05-24 12:38:03 -0700

    add url to allowed hosts

--- django_zimtracker/settings/production.py
+++ django_zimtracker/settings/production.py
@@ -2,7 +2,8 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['young-anchorage-71833.herokuapp.com',]
+ALLOWED_HOSTS = ['young-anchorage-71833.herokuapp.com',
+    'zimtracker.herokuapp.com']
 
 # STATIC_URL = 'https://{0}/{1}/'.format(
 #     AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
