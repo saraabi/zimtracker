0712571fb2b22e656441633cd6b253d8c570d91a
Samer Araabi <samer@Samers-Mac-mini.local>
2021-05-28 20:41:30 -0700

    add responsive design, home view post method

--- zimtracker/functions.py
+++ zimtracker/functions.py
@@ -2,6 +2,8 @@ from django.apps import apps
 from django.conf import settings
 
 from marinetrafficapi import MarineTrafficApi
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 from twilio.rest import Client
 
 from .models import Vessel
@@ -68,6 +70,20 @@ def send_updates(log):
         kwargs['to'] = user.phone
         client.messages.create(**kwargs)
 
-
-
+def send_email(to, subject, content):
+    message = Mail(
+        from_email='admin@zimtracker.herokuapp.com',
+        to_emails=to,
+        subject=subject,
+        html_content=content)
+    try:
+        api_key = get_settings_value('SENDGRID_API_KEY')
+        sg = SendGridAPIClient(api_key)
+        response = sg.send(message)
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+    except Exception as e:
+        print('ERROR')
+        print(e.message)
 
--- zimtracker/views.py
+++ zimtracker/views.py
@@ -6,20 +6,38 @@ from django.views.generic import View, DetailView, ListView
 from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic.edit import DeleteView
 
+from .functions import send_email
 from .models import Vessel, UserProfile
+
 
 class Home(View):
     template_name = 'home.html'
-
-    def get(self, request, **kwargs):
-        context = self.get_context_data()
-        return render(request, self.template_name, context)
 
     def get_context_data(self, **kwargs):
         context = {
             'vessel_list': Vessel.objects.all(),
         }
         return context
+
+    def get(self, request, **kwargs):
+        context = self.get_context_data()
+        return render(request, self.template_name, context)
+
+    def post(self, request, **kwargs):
+        context = self.get_context_data()
+        email = send_email(
+            to='sarrabi@gmail.com',
+            subject='ZimTracker Request',
+            content='{0} ({1} {2}): {3}'.format(
+                request.POST.get('name'),
+                request.POST.get('email'),
+                request.POST.get('phone'),
+                request.POST.get('location')
+            )
+        )
+        messages.success(request, 
+            'Request Received, Thank You!')
+        return render(request, self.template_name, context)
 
 class VesselList(ListView):
     model = Vessel
